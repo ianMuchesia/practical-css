a605c732f442b5138b50a820e9b520e7473dfd98
ianMuchesia <ianmuchesia@yahoo.com>
2023-02-24 16:51:34 +0300

    animation

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -7,7 +7,8 @@ import card1 from './Card-1.png'
 import card2 from './card2.png'
 import card3 from './card3.png'
 import card4 from './card9.png'
+import card5 from './Card5.png'
 import card6 from './group-6.png'
 export {
-    logo,portfolio,laptop, team, testimonial, card1, card2, card3, card4,  card6
+    logo,portfolio,laptop, team,card5, testimonial, card1, card2, card3, card4,  card6
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { portfolio } from '../assets'
+import { card5, portfolio } from '../assets'
+import { card1, card2, card3, card4,card6 } from '../assets'
 import './hero.css'
 const Hero = () => {
   return (
-    <section className='section-two'>
+    <section className=''>
         <div className='section-center'>
     <div className='hero-section'>
         <div className='hero'>
@@ -20,7 +21,14 @@ const Hero = () => {
             </div>
         </div>
         <div className="hero-image-section">
+
             <img src={portfolio} alt='portfolio'  className='hero-image'/>
+            <img src={card1} alt="card1"  className='card1'/>
+            <img src={card2} alt="card2" className='card2'/>
+            <img src={card3} alt="card3" className='card3'/>
+            <img src={card4} alt="card4" className='card4'/>
+            <img src={card5} alt="card5" className='card5'/>
+            <img src={card6} alt="card6" className='card6'/>
         </div>
         </div>
     </div>
