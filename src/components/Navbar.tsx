7d3ff3cf42d885af37c4fee0096856e9be1a81e5
ianMuchesia <ianmuchesia@yahoo.com>
2023-02-24 15:37:04 +0300

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,37 @@
-import React from 'react'
-import { logo } from '../assets'
-import './navbar.css'
+import React, { useState } from "react";
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import { logo } from "../assets";
+import "./navbar.css";
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-center">
-        <div className='navbar-header'>
-          <img src={logo} alt=""/>
+        <div className="navbar-header">
+          <img src={logo} alt="" />
           <h3>Portfolio</h3>
-
         </div>
-        <ul className='nav-links'>
+        <ul className="nav-links">
           {/* checkbox hack */}
           <input type="checkbox" id="checkbox_toggle" />
-          <label htmlFor="checkbox_toggle" className='toggler'>&#9776;</label>
-          <div className='menu'>
-          <li className='nav-link'>Home</li>
-          <li className='nav-link'>About</li>
-          <li className='nav-link'>Portfolio</li>
-          <li className='nav-link'><button>Contact</button></li>
+          <label htmlFor="checkbox_toggle" className="toggler" onClick={()=>{setToggle(prevToggle=>!prevToggle
+            )}}>
+            {toggle?<AiOutlineClose/>: <AiOutlineMenu/>}
+          </label>
+          <div className="menu">
+            <li className="nav-link">Home</li>
+            <li className="nav-link">About</li>
+            <li className="nav-link">Portfolio</li>
+            <li className="nav-link">
+              <button>Contact</button>
+            </li>
           </div>
         </ul>
       </div>
-    
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
